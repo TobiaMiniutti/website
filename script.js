c491c82f42053a064db7977bcbaa9846c1eeb674
Tobia Miniutti <174129879+TobiaMiniutti@users.noreply.github.com>
2025-06-20 19:22:59 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function updateClock() {
-  const now = new Date();
-  document.querySelector('.sbb-clock__hours').textContent = String(now.getHours()).padStart(2, '0');
-  document.querySelector('.sbb-clock__minutes').textContent = String(now.getMinutes()).padStart(2, '0');
-  document.querySelector('.sbb-clock__seconds').textContent = String(now.getSeconds()).padStart(2, '0');
-}
-
-setInterval(updateClock, 1000);
-updateClock();
