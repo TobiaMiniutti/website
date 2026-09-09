3304aecb2fd2ba69d8f358dbc09edd4926c93da2
Tobia Miniutti <174129879+TobiaMiniutti@users.noreply.github.com>
2026-09-09 18:37:39 +0200

    Delete src directory

--- src/entry-client.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import { App } from "./App";
-import { initializeConsentDefaults } from "./lib/consent";
-import "./styles.css";
-
-initializeConsentDefaults();
-document.documentElement.classList.remove("no-js");
-hydrateRoot(document.getElementById("root")!, <React.StrictMode><App url={window.location.pathname} /></React.StrictMode>);
